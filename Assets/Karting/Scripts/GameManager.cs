ed1c1763a0774d8d4524800f479cc0addb143f56
ÐMisha <lokitoki26@gmail.com>
2024-06-24 20:45:37 +0300

    ReadyToGo

---
+++ Assets/Karting/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GhostDriver ghostDriver;
+    public float bestLapTime = float.MaxValue;
+    public Transform finishLine; // Объект финишной линии
+    private float currentLapTime;
+
+    void Start()
+    {
+        // Убедитесь, что GhostDriver неактивен в начале
+        if (ghostDriver != null)
+        {
+            ghostDriver.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("GhostDriver не назначен в инспекторе!");
+        }
+    }
+
+    void Update()
+    {
+        // Обновление текущего времени круга
+        currentLapTime += Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == finishLine)
+        {
+            Debug.Log("Финишная линия пересечена");
+
+            // Проверка завершения круга
+            if (currentLapTime < bestLapTime)
+            {
+                bestLapTime = currentLapTime;
+                Debug.Log("Новый лучший круг: " + bestLapTime);
+                ghostDriver.StopRecording();
+                ghostDriver.SaveBestFrames();
+                ghostDriver.StartRecording();
+            }
+            currentLapTime = 0f;
+
+            // Активируем GhostDriver и запускаем воспроизведение
+            ghostDriver.gameObject.SetActive(true);
+            ghostDriver.StartPlaying();
+        }
+    }
+}
